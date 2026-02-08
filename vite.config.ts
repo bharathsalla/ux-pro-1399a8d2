2c071fb4037b7dda3fe0facd60d4cf6477670db4
bharathsalla <bharathsalla456@gmail.com>
2026-02-08 15:44:19 +0000

    fix vite config build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/ux-pro/",
 
@@ -9,7 +15,10 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
 
   resolve: {
     alias: {
